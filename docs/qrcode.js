b30536a8d4644769eeffd73f97994d6d7bd0c0ed
Simon Marquis <contact@simon-marquis.fr>
2019-05-08 18:09:51 +0200

    Remove buggy detection of "android" in the qrcode.js library

--- docs/qrcode.js
+++ docs/qrcode.js
@@ -155,23 +155,6 @@ var QRCode;
 		return typeof CanvasRenderingContext2D != "undefined";
 	}
 	
-	// android 2.x doesn't support Data-URI spec
-	function _getAndroid() {
-		var android = false;
-		var sAgent = navigator.userAgent;
-		
-		if (/android/i.test(sAgent)) { // android
-			android = true;
-			var aMat = sAgent.toString().match(/android ([0-9]\.[0-9])/i);
-			
-			if (aMat && aMat[1]) {
-				android = parseFloat(aMat[1]);
-			}
-		}
-		
-		return android;
-	}
-	
 	var svgDrawer = (function() {
 
 		var Drawing = function (el, htOption) {
@@ -279,27 +262,6 @@ var QRCode;
 			this._elCanvas.style.display = "none";			
 		}
 		
-		// Android 2.1 bug workaround
-		// http://code.google.com/p/android/issues/detail?id=5141
-		if (this._android && this._android <= 2.1) {
-	    	var factor = 1 / window.devicePixelRatio;
-	        var drawImage = CanvasRenderingContext2D.prototype.drawImage; 
-	    	CanvasRenderingContext2D.prototype.drawImage = function (image, sx, sy, sw, sh, dx, dy, dw, dh) {
-	    		if (("nodeName" in image) && /img/i.test(image.nodeName)) {
-		        	for (var i = arguments.length - 1; i >= 1; i--) {
-		            	arguments[i] = arguments[i] * factor;
-		        	}
-	    		} else if (typeof dw == "undefined") {
-	    			arguments[1] *= factor;
-	    			arguments[2] *= factor;
-	    			arguments[3] *= factor;
-	    			arguments[4] *= factor;
-	    		}
-	    		
-	        	drawImage.apply(this, arguments); 
-	    	};
-		}
-		
 		/**
 		 * Check whether the user's browser supports Data URI or not
 		 * 
@@ -351,7 +313,6 @@ var QRCode;
 		 */
 		var Drawing = function (el, htOption) {
     		this._bIsPainted = false;
-    		this._android = _getAndroid();
 		
 			this._htOption = htOption;
 			this._elCanvas = document.createElement("canvas");
@@ -563,7 +524,6 @@ var QRCode;
 			Drawing = svgDrawer;
 		}
 		
-		this._android = _getAndroid();
 		this._el = el;
 		this._oQRCode = null;
 		this._oDrawing = new Drawing(this._el, this._htOption);
@@ -590,12 +550,11 @@ var QRCode;
 	/**
 	 * Make the Image from Canvas element
 	 * - It occurs automatically
-	 * - Android below 3 doesn't support Data-URI spec.
 	 * 
 	 * @private
 	 */
 	QRCode.prototype.makeImage = function () {
-		if (typeof this._oDrawing.makeImage == "function" && (!this._android || this._android >= 3)) {
+		if (typeof this._oDrawing.makeImage == "function") {
 			this._oDrawing.makeImage();
 		}
 	};
