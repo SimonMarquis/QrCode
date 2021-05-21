029488c52bb0be5ebb1c436c9eb143a35f4e30d4
Simon Marquis <contact@simon-marquis.fr>
2021-05-21 11:36:48 +0200

    Inject Settings in base DecoderActivity

--- app/src/main/java/fr/smarquis/qrcode/ui/DecoderActivity.kt
+++ app/src/main/java/fr/smarquis/qrcode/ui/DecoderActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import fr.smarquis.qrcode.model.Theme
-import fr.smarquis.qrcode.settings.ThemeSetting
+import fr.smarquis.qrcode.settings.SettingsRepository
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
 
 open class DecoderActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var settings: SettingsRepository
 
     lateinit var customTheme: Theme
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         customTheme = runBlocking {
-            ThemeSetting.get(this@DecoderActivity).first()
+            settings.theme.first()
         }.also {
             AppCompatDelegate.setDefaultNightMode(it.value)
         }
