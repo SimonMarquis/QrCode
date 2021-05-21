eaf78f59f24b108f70105a5438938213e19def6d
Simon Marquis <contact@simon-marquis.fr>
2021-05-21 12:41:29 +0200

    Check for GooglePlayServices availability in Application's onCreate

--- app/src/main/java/fr/smarquis/qrcode/Application.kt
+++ app/src/main/java/fr/smarquis/qrcode/Application.kt
@@ -1,7 +1,15 @@
 package fr.smarquis.qrcode
 
 import dagger.hilt.android.HiltAndroidApp
+import fr.smarquis.qrcode.utils.checkGooglePlayServices
 
 
 @HiltAndroidApp
-class Application : android.app.Application()
+class Application : android.app.Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        checkGooglePlayServices(this)
+    }
+
+}
