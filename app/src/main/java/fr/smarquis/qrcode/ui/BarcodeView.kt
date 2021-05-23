6da1474488b2e02fe90e7b4435781c9bbea5a75b
Simon Marquis <contact@simon-marquis.fr>
2021-05-23 15:39:12 +0200

    Fix initial state of BarcodeView

--- app/src/main/java/fr/smarquis/qrcode/ui/BarcodeView.kt
+++ app/src/main/java/fr/smarquis/qrcode/ui/BarcodeView.kt
@@ -1,17 +1,15 @@
 package fr.smarquis.qrcode.ui
 
-import android.Manifest
 import android.content.Context
-import android.content.pm.PackageManager
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
-import androidx.core.content.ContextCompat
 import androidx.transition.TransitionManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_COLLAPSED
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_EXPANDED
 import fr.smarquis.qrcode.R
 import fr.smarquis.qrcode.databinding.ViewBarcodeBinding
 import fr.smarquis.qrcode.model.Barcode
@@ -24,6 +22,8 @@ class BarcodeView @JvmOverloads constructor(
 
     private val binding = ViewBarcodeBinding.inflate(LayoutInflater.from(context), this)
 
+    private val bottomSheetBehavior by lazy { BottomSheetBehavior.from(binding.root) }
+
     private lateinit var onCollapsed: () -> Unit
     private lateinit var open: (Barcode) -> Unit
     private lateinit var copy: (Barcode) -> Unit
@@ -31,8 +31,9 @@ class BarcodeView @JvmOverloads constructor(
 
     var barcode: Barcode? = null
         set(value) {
-            field = value
-            render(value)
+            field = value.also { if (it == field) return }
+            TransitionManager.beginDelayedTransition(this@BarcodeView)
+            if (value != null) render(value) else clear()
         }
 
     private val callback = object : BottomSheetBehavior.BottomSheetCallback() {
@@ -44,8 +45,6 @@ class BarcodeView @JvmOverloads constructor(
         }
     }
 
-    private fun bottomSheetBehavior() = BottomSheetBehavior.from(this)
-
     init {
         isClickable = true
         clipChildren = false
@@ -56,9 +55,9 @@ class BarcodeView @JvmOverloads constructor(
             copyImageView.setOnClickListener { barcode?.let { copy(it) } }
             headerLinearLayout.setOnClickListener {
                 barcode ?: return@setOnClickListener
-                bottomSheetBehavior().state = when (bottomSheetBehavior().state) {
-                    STATE_COLLAPSED -> BottomSheetBehavior.STATE_EXPANDED
-                    BottomSheetBehavior.STATE_EXPANDED -> STATE_COLLAPSED
+                bottomSheetBehavior.state = when (bottomSheetBehavior.state) {
+                    STATE_COLLAPSED -> STATE_EXPANDED
+                    STATE_EXPANDED -> STATE_COLLAPSED
                     else -> return@setOnClickListener
                 }
             }
@@ -69,12 +68,12 @@ class BarcodeView @JvmOverloads constructor(
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        bottomSheetBehavior().addBottomSheetCallback(callback)
+        bottomSheetBehavior.addBottomSheetCallback(callback)
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-        bottomSheetBehavior().removeBottomSheetCallback(callback)
+        bottomSheetBehavior.removeBottomSheetCallback(callback)
     }
 
     fun configure(onCollapsed: () -> Unit, more: (() -> Unit)? = null, open: (Barcode) -> Unit, copy: (Barcode) -> Unit) {
@@ -86,44 +85,34 @@ class BarcodeView @JvmOverloads constructor(
         this.copy = copy
     }
 
-    private fun render(value: Barcode?) = with(binding) {
-        when {
-            ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED -> {
-                bottomSheetBehavior().isHideable = true
-                bottomSheetBehavior().state = BottomSheetBehavior.STATE_HIDDEN
-            }
-            value == null -> {
-                bottomSheetBehavior().state = STATE_COLLAPSED
-                bottomSheetBehavior().isHideable = false
-                progressBar.visibility = View.VISIBLE
-                iconImageView.visibility = View.GONE
-                iconImageView.setImageResource(0)
-                titleTextView.setText(R.string.status_scanning)
-                openImageView.visibility = View.GONE
-                copyImageView.visibility = View.GONE
-                detailsTextView.visibility = View.GONE
-                rawTextView.visibility = View.GONE
-            }
-            else -> {
-                if (bottomSheetBehavior().state == BottomSheetBehavior.STATE_EXPANDED) TransitionManager.beginDelayedTransition(this@BarcodeView)
-                progressBar.visibility = View.GONE
-                iconImageView.setImageResource(value.icon)
-                iconImageView.visibility = View.VISIBLE
-                titleTextView.text = value.title
-                openImageView.visibility = if (value.intent != null) View.VISIBLE else View.GONE
-                copyImageView.visibility = View.VISIBLE
-                value.details.let {
-                    detailsTextView.visibility = if (it.isNullOrBlank()) View.GONE else View.VISIBLE
-                    detailsTextView.text = it
-                }
-                value.value.let {
-                    rawTextView.visibility = if (it.isBlank()) View.GONE else View.VISIBLE
-                    rawTextView.text = it
-                }
-                bottomSheetBehavior().state = BottomSheetBehavior.STATE_EXPANDED
-                bottomSheetBehavior().isHideable = false
-            }
+    private fun clear() = with(binding) {
+        bottomSheetBehavior.state = STATE_COLLAPSED
+        progressBar.visibility = View.VISIBLE
+        iconImageView.visibility = View.GONE
+        iconImageView.setImageResource(0)
+        titleTextView.setText(R.string.status_scanning)
+        openImageView.visibility = View.GONE
+        copyImageView.visibility = View.GONE
+        detailsTextView.visibility = View.GONE
+        rawTextView.visibility = View.GONE
+    }
+
+    private fun render(barcode: Barcode) = with(binding) {
+        progressBar.visibility = View.GONE
+        iconImageView.setImageResource(barcode.icon)
+        iconImageView.visibility = View.VISIBLE
+        titleTextView.text = barcode.title
+        openImageView.visibility = if (barcode.intent != null) View.VISIBLE else View.GONE
+        copyImageView.visibility = View.VISIBLE
+        barcode.details.let {
+            detailsTextView.visibility = if (it.isNullOrBlank()) View.GONE else View.VISIBLE
+            detailsTextView.text = it
         }
+        barcode.value.let {
+            rawTextView.visibility = if (it.isBlank()) View.GONE else View.VISIBLE
+            rawTextView.text = it
+        }
+        bottomSheetBehavior.state = STATE_EXPANDED
     }
 
 }
