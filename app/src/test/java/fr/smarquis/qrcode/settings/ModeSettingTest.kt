23dc33714dc681274b325670330c199e31b96a3a
Simon Marquis <contact@simon-marquis.fr>
2024-07-12 20:31:46 +0200

    Check Robolectric default values

--- app/src/test/java/fr/smarquis/qrcode/settings/ModeSettingTest.kt
+++ app/src/test/java/fr/smarquis/qrcode/settings/ModeSettingTest.kt
@@ -5,6 +5,7 @@ import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import fr.smarquis.qrcode.model.Mode
 import fr.smarquis.qrcode.model.Mode.AUTO
+import fr.smarquis.qrcode.model.Mode.MANUAL
 import fr.smarquis.qrcode.settings.ModeSetting.mapOut
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
@@ -28,17 +29,19 @@ class ModeSettingTest {
     }
 
     @Test
-    fun `mapOut with null`() = test(expected = AUTO, value = null)
+    fun `mapOut with null`() = test(expected = default(), value = null)
 
     @Test
-    fun `mapOut with empty`() = test(expected = AUTO, value = "")
+    fun `mapOut with empty`() = test(expected = default(), value = "")
 
     @Test
-    fun `mapOut with unsupported`() = test(expected = AUTO, value = "abc")
+    fun `mapOut with unsupported`() = test(expected = default(), value = "abc")
 
     @Test
     fun `mapOut with supported values`() = Mode.values().forEach {
         test(expected = it, value = it.name)
     }
+
+    private fun default() = if (context.packageManager.hasSystemFeature("android.hardware.touchscreen")) MANUAL else AUTO
 
 }
